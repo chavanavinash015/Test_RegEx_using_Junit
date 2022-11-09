861064b046ac91224ca3b7cfd3f904bdfe304322
arc-015 <chavanavinash015@gmail.com>
2022-11-09 19:32:50 +0530

    added test Email id  files

--- src/test/java/com/bridgelabz/junit/UserRegistration.java
+++ src/test/java/com/bridgelabz/junit/UserRegistration.java
@@ -19,10 +19,18 @@ public class UserRegistration {
         Boolean result =validation.checkFirstName("Avinash");
         Assertions.assertEquals(result,true);
     }
-
+    @Test
     void given_last_name_should_returnTrue(){
         UserValidation validation= new UserValidation();
         Boolean result1 = validation.checkLastName("Chavan");
         Assertions.assertEquals(result1,true);
     }
+
+    @Test
+    void given_email_id_should_returnTrue(){
+        UserValidation validation= new UserValidation();
+        Boolean result2 = validation.checkEmail("Avinashchavan015@gmail.com");
+        Assertions.assertEquals(result2,true);
+    }
+    
 }
